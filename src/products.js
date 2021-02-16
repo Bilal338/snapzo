f830fdcc86f140d6ba904574de73e7f8f9602c85
Bilal338 <21100263@lums.edu.pk>
2021-02-16 06:55:54 +0500

    first commit

---
+++ src/products.js
@@ -0,0 +1,17 @@
+import wedding from "./Wedding.jpg";
+import birthday from "./birthday.jfif";
+
+const products = [
+  {
+    name: "Wedding Photography",
+    price: 25000,
+    image: { wedding },
+  },
+  {
+    name: "Birthdayy Photography",
+    price: 10000,
+    image: { birthday },
+  },
+];
+
+export default products;
